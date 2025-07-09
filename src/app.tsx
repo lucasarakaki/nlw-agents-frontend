35966fd406325ad6909662652f0da47f26b1f0ed
lucasarakaki <takeshioficial01@gmail.com>
2025-07-09 17:23:01 -0300

    feat: audio recording and question creation

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { CreateRoom } from "./pages/create-room";
-import { Room } from "./pages/room";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { RecordRoomAudio } from './pages/record-room-audio'
+import { Room } from './pages/room'
 
-const queryClient = new QueryClient
+const queryClient = new QueryClient()
 
 export function App() {
   return (
-    <QueryClientProvider client={ queryClient }>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes> 
+        <Routes>
           <Route element={<CreateRoom />} index />
           <Route element={<Room />} path="/room/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
   )
 }
-
